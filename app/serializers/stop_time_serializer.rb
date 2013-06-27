294487173af3c0028969f389a0a655bd0499f5de
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-27 01:08:05 -0500

    format attributes for the stop_time serializer, return the id from gtfs instead of rails ids, format dates output and conditional output of the stop reference

--- app/serializers/stop_time_serializer.rb
+++ app/serializers/stop_time_serializer.rb
@@ -1,3 +1,26 @@
 class StopTimeSerializer < ActiveModel::Serializer
-  attributes :id, :trip_id, :trip_str, :arrival_time, :departure_time, :stop_id, :stop_sequence
+  attributes :id, :trip_id, :arrival_time, :departure_time, :stop_id, :stop_sequence
+  has_one :stop
+  has_one :trip
+
+  def stop_id
+    object.stop_str
+  end
+
+  def trip_id
+    object.trip_str
+  end
+
+  def departure_time
+    Time.parse(object.departure_time.to_s).strftime("%H:%M:%S")
+  end
+
+  def arrival_time
+    Time.parse(object.arrival_time.to_s).strftime("%H:%M:%S")
+  end
+
+  def include_stop?
+    object.include_stop
+  end
+
 end

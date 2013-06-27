55933346f4c6add9b49ebd95d9252e5bc2b623ba
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-27 01:04:46 -0500

    include on serializer stop_time reference depending of the value of the object

--- app/serializers/stop_serializer.rb
+++ app/serializers/stop_serializer.rb
@@ -1,3 +1,16 @@
 class StopSerializer < ActiveModel::Serializer
   attributes :id, :stop_id, :stop_code, :stop_name, :stop_lat, :stop_lon, :stop_type
+  has_many :stop_times
+
+
+  def include_stop_times?
+    object.include_stop_times
+  end
+
+  def stop_times
+    stop_times = object.stop_times.order("departure_time ASC")
+    stop_times.where("departure_time > ?", Time.now.to_s) if object.from_now
+    stop_times.limit(object.stop_time_limit) if object.stop_time_limit.present?
+  end
+
 end

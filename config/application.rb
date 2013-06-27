09a018200f0df3eb4225c8ce164888e43212ecd0
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-26 23:02:38 -0500

    only include what's needed on the API

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,8 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+# require "action_mailer/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems

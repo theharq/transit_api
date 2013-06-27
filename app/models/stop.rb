1e9d3833164866f65bee6758404584ff1f33ec56
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-27 08:13:53 -0500

    utility class with the haversine method and include it on the stop model

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -5,9 +5,24 @@ class Stop < ActiveRecord::Base
   def self.search(params)
     stops = self.limit(20)
     stops = stops.limit(params[:limit]) if params[:limit].present?
+
+    if params[:latitude].present? && params[:longitude].present?
+      stops = stops.near_to_location(params[:latitude], params[:longitude], params[:distance].present? ? params[:distance].to_i : 300 )
+    end
+
+
     stops.map {|s| s.from_now = true} if params[:from] == 'now'
     stops.map {|s| s.stop_time_limit = params[:stop_time_limit]} if params[:stop_time_limit].present?
     stops.map {|s| s.include_stop_times = true} if params[:include_stop_times] == 'true'
+
     stops
+
   end
+
+  def self.near_to_location(latitude,longitude,distance)
+
+
+    self.select{ |stop| Utility.haversine(stop.stop_lat, stop.stop_lon, latitude.to_f, longitude.to_f) <= distance }
+  end
+
 end

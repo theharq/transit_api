2d5d9cb09b021005b3804d5b95da25366f15028a
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-27 00:09:54 -0500

    class method search for stop model, limit the results

--- app/controllers/api/v1/stops_controller.rb
+++ app/controllers/api/v1/stops_controller.rb
@@ -3,11 +3,18 @@ module Api
     class StopsController < ApplicationController
 
       def index
-        render json: Stop.all
+        render json: Stop.search(stop_params)
       end
 
       def show
         render json: Stop.find(params[:id])
+      end
+
+
+      private
+
+      def stop_params
+        params.permit(:limit, :latitude, :longitude, :distance)
       end
     end
   end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,3 +1,9 @@
 class Stop < ActiveRecord::Base
   has_many :stop_times
+
+  def self.search(params)
+    stops = self.limit(20)
+    stops = stops.limit(params[:limit]) if params[:limit].present?
+    stops
+  end
 end

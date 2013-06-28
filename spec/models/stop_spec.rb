944d82a3acb988f6433b18449de40a56b81d32af
Hildebrando Rueda <harq.soft@gmail.com>
2013-06-28 00:30:34 -0500

    test for near_to_location class method on Stop model

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -3,3 +3,16 @@ require 'spec_helper'
 describe Stop, "Relations" do
   it { should have_many :stop_times }
 end
+
+describe Stop, "Class Methods" do
+  describe ".near_to_location" do
+    before(:each) do
+      FactoryGirl.create(:stop, stop_lat: 11.002337, stop_lon: -74.809367)
+      FactoryGirl.create(:stop, stop_lat: 48.856614, stop_lon: 2.3522219)
+    end
+
+    it "should return only one stop" do
+      Stop.near_to_location(11.00353,-74.81143, 300).size.should == 1
+    end
+  end
+end
